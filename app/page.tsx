5ee79cad9d562fca2df08837b2ccd6c3f753ed1c
yohansngus <yohansngus8@gmail.com>
2026-05-08 07:53:24 -0700

    New update

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,167 @@
-import Image from "next/image";
-
+"use client";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Legend,
+  PieChart,
+  Pie,
+  Cell,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 export default function Home() {
+  const lineData = [
+    { month: "Jan", amount: 150 },
+    { month: "Feb", amount: 310 },
+    { month: "Mar", amount: 5 },
+    { month: "Apr", amount: 30 },
+    { month: "May", amount: 80 },
+    { month: "Jun", amount: 50 },
+    { month: "Jul", amount: 100 },
+  ];
+  const pieData = [
+    { category: "Rent", amount: 150 },
+    { category: "Food", amount: 310 },
+    { category: "Transport", amount: 10 },
+    { category: "Entertainment", amount: 30 },
+    { category: "Education", amount: 80 },
+    { category: "Healthcare", amount: 51 },
+    { category: "Fashion", amount: 100 },
+  ];
+  const COLORS = [
+    "#635bff",
+    "#0ea5e9",
+    "#10b981",
+    "#f59e0b",
+    "#ef4444",
+    "#8b5cf6",
+  ];
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <div className="font-sans bg-white pb-10">
+      {/*Nav bar*/}
+      <div className="flex navbar bg-base-100 shadow-sm">
+        <div className="flex-1">
+          <h1 className="flex m-5 h-20 w-30 text-center items-center justify-center rounded-lg bg-black text-white font-bold shadow-md">
+            Expense Tracker
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+        <div className="flex flex-row gap-2 h-15 m-5">
+          <div className="w-15 rounded-2xl border-4 border-gray-500">
+            <img
+              className="rounded-xl"
+              alt="user profile picture"
+              src="/assets/newup.jpg"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
+        </div>
+      </div>
+      {/*Main content*/}
+      <main className="flex flex-col w-full flex-col p-6">
+        <h1 className="text-3xl font-bold shadow-sm p-8 my-15 text-black rounded-xl">
+          Expense Tracker Dashboard
+        </h1>
+        {/*cards section*/}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-6">
+          <div className="bg-white p-6 rounded-2xl shadow-sm">
+            <p className="text-gray-500 text-sm">Total Spending</p>
+
+            <h2 className="text-3xl text-gray-400 font-bold mt-2">$715</h2>
+          </div>
+
+          <div className="bg-white p-6 rounded-2xl shadow-sm">
+            <p className="text-gray-500 text-sm">Highest Category</p>
+
+            <h2 className="text-2xl text-gray-400 font-bold mt-2">FOOD</h2>
+          </div>
+
+          <div className="bg-white p-6 rounded-2xl shadow-sm">
+            <p className="text-gray-500 text-sm">Monthly Growth</p>
+
+            <h2 className="text-2xl font-bold mt-2 text-green-500">+12%</h2>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 w-full">
+          {" "}
+          <div className="w-full h-[400px] bg-white p-6 rounded-2xl shadow-sm">
+            <h2 className="text-lg text-gray-300 font-semibold mb-4 pl-2">
+              Monthly Spending Trend
+            </h2>
+
+            <ResponsiveContainer width="100%" height="100%">
+              <LineChart
+                data={lineData}
+                margin={{
+                  top: 20,
+                  right: 20,
+                  bottom: 5,
+                  left: 0,
+                }}
+              >
+                <CartesianGrid stroke="#aaa" strokeDasharray="5 5" />
+                <Line
+                  type="monotone"
+                  dataKey="amount"
+                  stroke="#635bff"
+                  strokeWidth={3}
+                  name="Expense"
+                />
+                <XAxis tick={{ fill: "#00000086" }} dataKey="month" />
+
+                <YAxis
+                  tick={{ fill: "#00000062" }}
+                  dataKey="amount"
+                  width="auto"
+                />
+                <Tooltip
+                  contentStyle={{
+                    borderRadius: "12px",
+                    color: "rgba(0, 0, 0, 0.66)",
+                    border: "1px solid #e5e7eb",
+                    backgroundColor: "#ffffff",
+                    boxShadow: "0 4px 12px rgba(0,0,0,0.08)",
+                  }}
+                />
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
+          <div className="flex justify-center border-2 border-gray-300 rounded-xl h-[400px]">
+            <h2 className="text-gray-300 text-lg font-semibold mb-4 pl-2">
+              Expense Categories
+            </h2>
+            <ResponsiveContainer width="100%" height="100%">
+              <PieChart>
+                <Pie
+                  data={pieData}
+                  dataKey="amount"
+                  nameKey="category"
+                  cx="50%"
+                  cy="50%"
+                  outerRadius={120}
+                  fill="#8884d8"
+                >
+                  {pieData.map((entry, index) => (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={COLORS[index % COLORS.length]}
+                    />
+                  ))}
+                </Pie>
+                <Tooltip
+                  contentStyle={{
+                    borderRadius: "12px",
+                    border: "none",
+                    color: "#fff",
+                    textShadow: "2px 2px 4px rgba(0,0,0,0.5)",
+                    background: "black",
+                  }}
+                />
+                <Legend align="left" />
+              </PieChart>
+            </ResponsiveContainer>
+          </div>
         </div>
       </main>
     </div>
