1cb3e9ac74f44100b028654b35ef5a735ac6892f
yohansngus <yohansngus8@gmail.com>
2026-05-11 04:45:14 -0700

    Improve expense tracker: add delete, localStorage, dynamic charts, and empty states

--- app/page.tsx
+++ app/page.tsx
@@ -12,26 +12,8 @@ import {
   Tooltip,
   ResponsiveContainer,
 } from "recharts";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 export default function Home() {
-  const lineData = [
-    { month: "Jan", amount: 150 },
-    { month: "Feb", amount: 310 },
-    { month: "Mar", amount: 5 },
-    { month: "Apr", amount: 30 },
-    { month: "May", amount: 80 },
-    { month: "Jun", amount: 50 },
-    { month: "Jul", amount: 100 },
-  ];
-  const pieData = [
-    { category: "Rent", amount: 150 },
-    { category: "Food", amount: 310 },
-    { category: "Transport", amount: 10 },
-    { category: "Entertainment", amount: 32 },
-    { category: "Education", amount: 80 },
-    { category: "Healthcare", amount: 51 },
-    { category: "Fashion", amount: 100 },
-  ];
   const COLORS = [
     "#635bff",
     "#0ea5e9",
@@ -65,6 +47,37 @@ export default function Home() {
     setAmount("");
   };
 
+  // getting data from local storage
+
+  useEffect(() => {
+    const savedExpenses = localStorage.getItem("expenses");
+
+    if (savedExpenses) {
+      setExpenses(JSON.parse(savedExpenses));
+    }
+  }, []);
+
+  // setting data to local storage
+
+  useEffect(() => {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [expenses]);
+
+  const pieData = expenses.map((item) => ({
+    expense: item.expense,
+    amount: item.amount,
+  }));
+
+  const lineData = expenses.map((item, index) => ({
+    month: item.expense,
+    amount: item.amount,
+  }));
+
+  const highestExpense = expenses.reduce(
+    (max, item) => (item.amount > max.amount ? item : max),
+    { expense: "", amount: 0 },
+  );
+
   return (
     <div className="font-sans bg-white pb-10">
       {/*Nav bar*/}
@@ -93,7 +106,7 @@ export default function Home() {
               placeholder="Expense name"
               value={expense}
               onChange={(e) => setExpense(e.target.value)}
-              className="w-full border p-2 rounded"
+              className="w-full border border-gray-300 p-2 rounded text-gray-500"
             />
 
             <input
@@ -101,22 +114,33 @@ export default function Home() {
               placeholder="Amount"
               value={amount}
               onChange={(e) => setAmount(e.target.value)}
-              className="w-full border p-2 rounded"
+              className="w-full border border-gray-300 p-2 rounded text-gray-500"
             />
 
             <button
               type="submit"
-              className="bg-black text-white px-4 py-2 rounded"
+              className="bg-gray-800 text-white px-4 py-2 rounded hover:bg-gray-700 cursor-pointer"
             >
               Add Expense
             </button>
           </form>
-
           <div className="mt-6">
             {expenses.map((item) => (
               <div key={item.id} className="flex justify-between border-b py-2">
-                <span>{item.expense}</span>
-                <span>${item.amount}</span>
+                <span className="text-gray-500">{item.expense}</span>
+
+                <div className="flex items-center gap-3">
+                  <span className="text-gray-500">${item.amount}</span>
+
+                  <button
+                    onClick={() =>
+                      setExpenses(expenses.filter((exp) => exp.id !== item.id))
+                    }
+                    className="text-red-500 font-bold cursor-pointer"
+                  >
+                    X
+                  </button>
+                </div>
               </div>
             ))}
           </div>
@@ -130,9 +154,11 @@ export default function Home() {
           </div>
 
           <div className="bg-white p-6 rounded-2xl shadow-sm">
-            <p className="text-gray-500 text-sm">Highest Category</p>
-
-            <h2 className="text-2xl text-gray-400 font-bold mt-2">FOOD</h2>
+            {" "}
+            <p className="text-gray-500 text-sm">Highest expense</p>{" "}
+            <h2 className="text-2xl text-gray-400 font-bold mt-2">
+              {highestExpense.expense || "No expenses"}
+            </h2>
           </div>
 
           <div className="bg-white p-6 rounded-2xl shadow-sm">
@@ -147,78 +173,85 @@ export default function Home() {
             <h2 className="text-lg text-gray-300 font-semibold mb-4 pl-2">
               Monthly Spending Trend
             </h2>
-
-            <ResponsiveContainer width="100%" height="100%">
-              <LineChart
-                data={lineData}
-                margin={{
-                  top: 20,
-                  right: 20,
-                  bottom: 5,
-                  left: 0,
-                }}
-              >
-                <CartesianGrid stroke="#aaa" strokeDasharray="5 5" />
-                <Line
-                  type="monotone"
-                  dataKey="amount"
-                  stroke="#635bff"
-                  strokeWidth={3}
-                  name="Expense"
-                />
-                <XAxis tick={{ fill: "#00000086" }} dataKey="month" />
-
-                <YAxis
-                  tick={{ fill: "#00000062" }}
-                  dataKey="amount"
-                  width="auto"
-                />
-                <Tooltip
-                  contentStyle={{
-                    borderRadius: "12px",
-                    color: "rgba(0, 0, 0, 0.66)",
-                    border: "1px solid #e5e7eb",
-                    backgroundColor: "#ffffff",
-                    boxShadow: "0 4px 12px rgba(0,0,0,0.08)",
+            {expenses.length > 0 ? (
+              <ResponsiveContainer width="100%" height="100%">
+                <LineChart
+                  data={lineData}
+                  margin={{
+                    top: 20,
+                    right: 20,
+                    bottom: 5,
+                    left: 0,
                   }}
-                />
-              </LineChart>
-            </ResponsiveContainer>
+                >
+                  <CartesianGrid stroke="#aaa" strokeDasharray="5 5" />
+                  <Line
+                    type="monotone"
+                    dataKey="amount"
+                    stroke="#635bff"
+                    strokeWidth={3}
+                    name="Expense"
+                  />
+                  <XAxis tick={{ fill: "#00000086" }} dataKey="month" />
+
+                  <YAxis tick={{ fill: "#00000062" }} width="auto" />
+                  <Tooltip
+                    contentStyle={{
+                      borderRadius: "12px",
+                      color: "rgba(0, 0, 0, 0.66)",
+                      border: "1px solid #e5e7eb",
+                      backgroundColor: "#ffffff",
+                      boxShadow: "0 4px 12px rgba(0,0,0,0.08)",
+                    }}
+                  />
+                </LineChart>
+              </ResponsiveContainer>
+            ) : (
+              <div className="flex items-center justify-center h-full text-gray-400">
+                No data available
+              </div>
+            )}
           </div>
           <div className="flex justify-center border-2 border-gray-300 rounded-xl h-[400px]">
             <h2 className="text-gray-300 text-lg font-semibold mb-4 pl-2">
               Expense Categories
             </h2>
-            <ResponsiveContainer width="100%" height="100%">
-              <PieChart>
-                <Pie
-                  data={pieData}
-                  dataKey="amount"
-                  nameKey="category"
-                  cx="50%"
-                  cy="50%"
-                  outerRadius={120}
-                  fill="#8884d8"
-                >
-                  {pieData.map((entry, index) => (
-                    <Cell
-                      key={`cell-${index}`}
-                      fill={COLORS[index % COLORS.length]}
-                    />
-                  ))}
-                </Pie>
-                <Tooltip
-                  contentStyle={{
-                    borderRadius: "12px",
-                    border: "none",
-                    color: "#fff",
-                    textShadow: "2px 2px 4px rgba(0,0,0,0.5)",
-                    background: "black",
-                  }}
-                />
-                <Legend align="left" />
-              </PieChart>
-            </ResponsiveContainer>
+            {expenses.length > 0 ? (
+              <ResponsiveContainer width="100%" height="100%">
+                <PieChart>
+                  <Pie
+                    data={pieData}
+                    dataKey="amount"
+                    nameKey="expense"
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={120}
+                    fill="#8884d8"
+                  >
+                    {pieData.map((entry, index) => (
+                      <Cell
+                        key={`cell-${index}`}
+                        fill={COLORS[index % COLORS.length]}
+                      />
+                    ))}
+                  </Pie>
+                  <Tooltip
+                    contentStyle={{
+                      borderRadius: "12px",
+                      border: "none",
+                      color: "#fff",
+                      textShadow: "2px 2px 4px rgba(0,0,0,0.5)",
+                      background: "black",
+                    }}
+                  />
+                  <Legend align="left" />
+                </PieChart>
+              </ResponsiveContainer>
+            ) : (
+              <div className="flex items-center justify-center h-full text-gray-400">
+                No expenses yet
+              </div>
+            )}
           </div>
         </div>
       </main>
